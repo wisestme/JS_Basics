649debd8f8fc34a89829e55a956e55fd33fd4a45
Chijioke Nwagwu <wyhzest@gmail.com>
2019-08-23 19:58:42 +0100

    calculates individual BMIs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 console.log("Test link");
 
 // CODING CHALLENGE
+// BMI comparator
+// BMI = mass/height^2
+
+// variables declaration
+
 let markHeight, markMass,
 	johnHeight, johnMass,
 	markBMI, johnBMI;
 
-	
+// BMI Calculation
+
+markBMI = markMass/(markHeight * markHeight);
+johnBMI = johnMass/(johnHeight * johnHeight);
+
