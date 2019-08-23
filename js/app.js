7056f0c2c565cc48d1d58c223568803e6b2d0a23
Chijioke Nwagwu <wyhzest@gmail.com>
2019-08-23 19:03:09 +0100

    calculates birth year

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log("Test link");
+
+// OPERATORS
+
+// MATH OPERATORS
+
+let wisestBirthYear, currentYear, ageWisest;
+ageWisest = 32;
+currentYear = 2019;
+wisestBirthYear = currentYear - ageWisest;
+
+console.log(wisestBirthYear);
