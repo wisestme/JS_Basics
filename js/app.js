da9f610f251d74546edff4659284849279cc346c
Chijioke Nwagwu <wyhzest@gmail.com>
2019-08-23 18:14:42 +0100

    deletes test codes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
 console.log("Test link");
-const firstName = 'Wisest';
-console.log(firstName);
-
-const lastName = 'Nwa Jesus';
-const age = 32;
-const fullAge = true;
-
-let job;
-job = 'Programmer';
-
-const isMarried = false;
-
-alert(`${firstName} is a ${age} year old ${job}. Is he married? ${isMarried}`);
-
-const middleName = prompt(`What is your middle name?`);
-alert(`My name is ${firstName} ${middleName} ${lastName}`)
