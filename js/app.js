8378a425f44b0ba4dac1696c54c8d7c2de50a059
Chijioke Nwagwu <wyhzest@gmail.com>
2019-09-02 15:24:03 +0100

    variable assignment using the ternary operator

--- js/app.js
+++ js/app.js
@@ -50,3 +50,5 @@ let firstName = 'Sammy';
 let age = 16;
 
 age >= 18 ? console.log(`${firstName} drinks beer.`) : console.log(`${firstName} drinks juice`);
+
+let drink = age >= 18 ? 'beer' : 'juice';
