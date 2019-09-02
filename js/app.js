3c45f43a14cc971b3489faf0a3c615993cb182f0
Chijioke Nwagwu <wyhzest@gmail.com>
2019-09-02 14:52:19 +0100

    modifies the condition

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ const age = 20;
 
 if (age < 13) {
 	console.log(`${firstName} is a boy`);
-} else if (age < 20) {
+} else if (age >= 13 && age <20) {
 	console.log(`${firstName} is a teenager`);
 } else {
 	console.log(`${firstName} is a man`);
