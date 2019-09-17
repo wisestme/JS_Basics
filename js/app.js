5278e289a5eed168d80ad75a7e0c3351069df810
wisestMe <wyhzest@gmail.com>
2019-09-17 10:58:04 +0100

    implements a for loop

--- js/app.js
+++ js/app.js
@@ -234,39 +234,7 @@ console.log(john);*/
 /**********************************************************
 * CODING CHALLENGE
 */
-/*
-// BMI comparator
-// BMI = mass/height^2
-
-// variables declaration
-
-let markHeight, markMass,
-	johnHeight, johnMass,
-	markBMI, johnBMI,
-	markBMIGreater;
-
-// variable assignment
-markHeight = 1.86;
-markMass = 60;
-johnHeight = 1.56;
-johnMass = 65;
-
-
-// BMI Calculation
-
-markBMI = markMass/(markHeight * markHeight);
-johnBMI = johnMass/(johnHeight * johnHeight);
-
-// Compare for Greater BMI
-
-markBMIGreater = markBMI > johnBMI;
-
-// Output result to the console
-
-console.log(`Is Mark's BMI higher than John's ${markBMIGreater}`)
-*/
-
-let mark = {
+/*let mark = {
 	firstName: 'Mark',
 	height: 1.80,
 	mass: 60,
@@ -293,4 +261,14 @@ if(john.BMI === mark.BMI) {
 	console.log(`${john.firstName} and ${mark.firstName} have equal BMI`)
 } else {
 	console.log(`${higherBMI} has a higher BMI`);
+}*/
+
+/**********************************************************
+* LOOPS AND ITERATION
+*/
+for (let i = 0; i < 6; i++) {
+	console.log('Chiji');
+	console.log('me');
 }
+
+console.log('him');
